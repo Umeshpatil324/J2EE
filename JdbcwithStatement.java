2629ac402fd36fefe823f28c9e9be684adbb6548
Umeshpatil324 <uc2073593@gmail.com>
2025-12-26 00:42:51 +0530

    Add files via upload

---
+++ JdbcwithStatement.java
@@ -0,0 +1,47 @@
+package Day1_jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JdbcwithStatement {
+	public static void main(String[] args) throws Exception {
+
+		JdbcwithStatement d = new JdbcwithStatement();
+//		d.insertData();
+		d.getAllRecord();
+
+	}
+
+	public void insertData() throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/batch416", "root", "Umesh@2006");
+		Statement s = c.createStatement();
+		int check = s.executeUpdate("insert into Student(id,name,city)values(22,'ram','goa');");
+
+		if (check > 0) {
+			System.out.println("Data is inserted...");
+		} else {
+			System.err.println("Data is NOT inserted...");
+		}
+
+		c.close();
+	}
+
+	public void getAllRecord() throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/batch416", "root", "Umesh@2006");
+		Statement s = c.createStatement();
+		ResultSet rs = s.executeQuery("select * from Student");
+
+		while (rs.next()) {
+			System.out.println("Student ID : " + rs.getInt(1) + "  Student Name : " + rs.getString(2)
+					+ " Student City :  " + rs.getString(3));
+		}
+
+		c.close();
+	}
+}
